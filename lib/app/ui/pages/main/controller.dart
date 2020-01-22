91f34ceeff19e55ddec20cbef2cbd46651e4bf7c
Maulana <mprambadi@gmail.com>
2020-01-23 06:06:58 +0700

    add main controller

---
+++ lib/app/ui/pages/main/controller.dart
@@ -0,0 +1,25 @@
+import 'package:clean_architecture_flutter/app/infrastucture/contracts/base_controller.dart';
+import 'package:clean_architecture_flutter/app/misc/user_data.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+class MainController extends BaseController {
+  @override
+  void initListeners() {
+    super.initListeners();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void onInternetConnected() {
+    // super.onInternetConnected();
+  }
+
+  @override
+  void onInternetDisconnected() {
+    // super.onInternetDisconnected();
+  }
+}
