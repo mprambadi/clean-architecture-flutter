193baf7e8b16fc0542cea52cb22705dbe9b8bad3
Maulana <mprambadi@gmail.com>
2020-01-23 16:26:52 +0700

    add post mapper di

--- lib/app/infrastucture/di/mapper_module.dart
+++ lib/app/infrastucture/di/mapper_module.dart
@@ -1,4 +1,5 @@
 
+import 'package:clean_architecture_flutter/data/persistences/mappers/post_mapper.dart';
 import 'package:clean_architecture_flutter/data/persistences/mappers/user_mapper.dart';
 import 'package:injector/injector.dart';
 
@@ -6,6 +7,7 @@ class MapperModule {
   static init(Injector injector){
     
     injector.registerSingleton<UserMapper>((_)=> UserMapper());
+    injector.registerSingleton<PostMapper>((_)=> PostMapper());
     
   }
 }
