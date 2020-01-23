d3d7073df812aa98331cbfef483a6d7d4362fc9f
Maulana <mprambadi@gmail.com>
2020-01-23 16:28:15 +0700

    add post usecase di

--- lib/app/infrastucture/di/usecase_module.dart
+++ lib/app/infrastucture/di/usecase_module.dart
@@ -1,5 +1,7 @@
 
+import 'package:clean_architecture_flutter/app/repository/api/post_api_repository.dart';
 import 'package:clean_architecture_flutter/app/repository/api/user_api_repository.dart';
+import 'package:clean_architecture_flutter/use_cases/post/get_post.dart';
 import 'package:clean_architecture_flutter/use_cases/user/get_user.dart';
 import 'package:injector/injector.dart';
 
@@ -8,6 +10,9 @@ class UsecaseModule {
 
     injector.registerDependency<GetUserUseCase>((Injector injector){
       return GetUserUseCase(injector.getDependency<UserApiRepository>());
+    });
+    injector.registerDependency<GetPostUseCase>((Injector injector){
+      return GetPostUseCase(injector.getDependency<PostApiRepository>());
     });
     
   }
