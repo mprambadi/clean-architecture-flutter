5c42a70e5425bbd6fdacd55aa7d3e2cf09079428
Maulana <mprambadi@gmail.com>
2020-01-23 14:25:07 +0700

    integrate user api repository di

--- lib/app/repository/api/user_api_repository.dart
+++ lib/app/repository/api/user_api_repository.dart
@@ -1,19 +1,17 @@
 
 // Here for the business logic of the entity
 import 'package:clean_architecture_flutter/app/infrastucture/endpoint.dart';
-import 'package:clean_architecture_flutter/app/infrastucture/persistences/api_services.dart';
 import 'package:clean_architecture_flutter/data/infrastructures/api_services_interface.dart';
 import 'package:clean_architecture_flutter/data/persistences/mappers/user_mapper.dart';
 import 'package:clean_architecture_flutter/data/persistences/repositories/contracts/user_repository.dart';
 import 'package:clean_architecture_flutter/domains/entities/user.dart';
-import 'package:dio/dio.dart';
 
 class UserApiRepository implements UserRepository {
   UserMapper _mapper;
   ApiServiceInterface _service;
   Endpoints _endpoints;
 
-  UserApiRepository(ApiService service, Endpoints endpoints, UserMapper mapper) {
+  UserApiRepository(ApiServiceInterface service, Endpoints endpoints, UserMapper mapper) {
     _mapper = mapper;
     _service = service;
     _endpoints = endpoints;
