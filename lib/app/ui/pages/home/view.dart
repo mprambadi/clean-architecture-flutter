12cbc9059f9ae84e66c866b8eb33881b2040b451
Maulana <mprambadi@gmail.com>
2020-01-23 14:26:08 +0700

    add home view di

--- lib/app/ui/pages/home/view.dart
+++ lib/app/ui/pages/home/view.dart
@@ -1,25 +1,17 @@
-import 'package:clean_architecture_flutter/app/infrastucture/endpoint.dart';
-import 'package:clean_architecture_flutter/app/infrastucture/persistences/api_services.dart';
-import 'package:clean_architecture_flutter/app/repository/api/user_api_repository.dart';
+import 'package:clean_architecture_flutter/app/infrastucture/app_component.dart';
 import 'package:clean_architecture_flutter/app/ui/pages/home/controller.dart';
-import 'package:clean_architecture_flutter/data/persistences/mappers/user_mapper.dart';
-import 'package:dio/dio.dart';
-import 'package:event_bus/event_bus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
 class HomePage extends View {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState(
+      AppComponent.getInjector().getDependency<HomeController>());
 }
 
 class _HomePageState extends ViewState<HomePage, HomeController> {
-  _HomePageState()
-      : super(HomeController(UserApiRepository(
-            new ApiService(Dio(), EventBus()),
-            Endpoints("https://jsonplaceholder.typicode.com"),
-            UserMapper())));
+  _HomePageState(HomeController homeController) : super(homeController);
 
   @override
   Widget buildPage() {
@@ -28,10 +20,15 @@ class _HomePageState extends ViewState<HomePage, HomeController> {
     return Column(
       children: <Widget>[
         RaisedButton(
-          onPressed: (){},
+          onPressed: () {},
           child: Text("hello"),
         ),
-        Expanded(child: ListView(children: usersList))
+        controller.isLoading
+            ? CircularProgressIndicator(backgroundColor: Colors.red)
+            : Expanded(
+                child: ListView(
+                children: usersList,
+              ))
       ],
     );
   }
